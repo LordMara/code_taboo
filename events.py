26b3c554961a39b803071008c410559d4945e0c7
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-23 00:09:55 +0200

    Add abc import, change Event class to abstract class, add docstrings to Event class

--- events.py
+++ events.py
@@ -1,36 +1,70 @@
-class Event():
+from abc import ABCMeta
+
+
+class Event(metaclass=ABCMeta):
+    """
+    Abstract class representing events
+
+    Attributes:
+        events (list of :obj: `Events`): list of all Events objects
+    """
 
     events = []
 
     def __init__(self, date):
-        '''date: Date object
+        '''
+        Construct Event object
+
+        Args:
+            date (:obj: `date`): date of event
         '''
 
         self.date = date
 
     def get_date(self):
+        """
+        Return date of event
+        """
+
         return self.date
 
     @classmethod
     def sort_events(cls):
+        """
+        Insertion sort alghoritm of sorting all events by date on events list
+        """
+
         is_sorted = False
 
         while not is_sorted and len(cls.events) > 1:
             is_sorted = True
+
             for i in range(len(cls.events) - 1):
                 if cls.events[i].date > cls.events[i+1].date:
                     temp = cls.events[i]
                     cls.events[i] = cls.events[i+1]
                     cls.events[i+1] = temp
+
                     is_sorted = False
 
     @classmethod
     def add_event(cls, event):
+        """
+        Add event to events list and call function to rot that list
+
+        Args:
+            event (:obj: `Event`): some event
+        """
+
         cls.events.append(event)
         cls.sort_events()
 
     @classmethod
     def get_events(cls):
+        """
+        Return list of events
+        """
+
         return cls.events
 
 
