a0c296cf564b511916250495f8ab7db2bdb5f8fa
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-23 18:08:07 +0200

    Add read_users() class method in class Mentor

--- models/mentor.py
+++ models/mentor.py
@@ -1,4 +1,6 @@
-from models.user import user
+import csv
+
+from models.user import User
 
 
 class Mentor(User):
@@ -6,3 +8,24 @@ class Mentor(User):
         super().__init__(name, surname, user_id, password)
 
         User.add_user(self)
+
+    @classmethod
+    def read_users(cls, file_name="csv/users.csv"):
+        """
+        Read User objects from csv of given name and add them to list of users
+
+        Args:
+            file_name (string): name of csv file
+        """
+
+        with open(file_name, "r", newline="") as csvfile:
+            file_reader = csv.reader(csvfile, delimiter="|")
+
+            for row in file_reader:
+                name = row[1]
+                surname = row[2]
+                user_id = row[3]
+                password = row[4]
+
+                if row[0] == cls.__name__:
+                    cls(name, surname, user_id, password)
