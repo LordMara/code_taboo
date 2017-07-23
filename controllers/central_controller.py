4a8bc47426873c1f59acab856044d02ec914a756
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-07-23 17:35:36 +0200

    Create central controler module with start_controller() and login_user() functions

---
+++ controllers/central_controller.py
@@ -0,0 +1,43 @@
+from models.mentor import Mentor
+from models.user import User
+from models.student import Student
+
+from views import view
+
+
+def start_controller():
+    """
+    Contain main logic of controller,
+    call functions to perform task choosen by user
+
+    Raises:
+        FileNotFoundError: if file to open is not present
+    """
+
+    try:
+        User.read_users()
+    except FileNotFoundError as err:
+        view.print_msg(err)
+    else:
+
+        user_id = view.get_user_id()
+        user_password = view.get_user_password()
+
+        try:
+            user = login_user()
+        except ValueError as err:
+            view.print_msg(err)
+        else:
+            if user.__class__.__name__ == "Student":
+                pass
+
+            elif user.__class__.__name__ == "Mentor":
+                pass
+
+
+def login_user():
+    for user in User.users_list:
+        if user_id == user.user_id and user_password == user.passowrd:
+            return user
+        else:
+            raise ValueError("Wrong user id or password!")
